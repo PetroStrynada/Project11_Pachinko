59f4920ae84a1ce773a4209e50b9203fa6351a1f
Petro Strynada <petro.strynada@gmail.com>
2023-07-26 00:28:50 +0300

    Bouncing balls: circleOfRadius
    + changed rectangles on balls
    + add bouncers with functions that allows adding lines that always
    starting at position of 0 xPixel

--- Project11_Pachinko/Project11_Pachinko/GameScene.swift
+++ Project11_Pachinko/Project11_Pachinko/GameScene.swift
@@ -9,7 +9,7 @@ import SpriteKit
 
 class GameScene: SKScene {
 
-override func didMove(to view: SKView) {
+    override func didMove(to view: SKView) {
         // Create a sprite node with the "background" image
         let backgroundNode = SKSpriteNode(imageNamed: "background")
         backgroundNode.position = CGPoint(x: 1133, y: 744)
@@ -28,15 +28,36 @@ override func didMove(to view: SKView) {
 
         // Add physics borders for screen edges
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+
+        
+        addLineOfBouncers(count: 13, stepInPixels: 206, yCoordinateInPixel: 0)
+
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
 
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+
+    func addBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+
+    func addLineOfBouncers(count: Int, stepInPixels: Int, yCoordinateInPixel: Int ) {
+        var total = 0
+        for _ in 0...count {
+            addBouncer(at: CGPoint(x: stepInPixels * total, y: yCoordinateInPixel))
+            total += 1
+        }
     }
 }
