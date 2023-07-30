b57333631f0884d8095e9f6468895d19a038ac56
Petro Strynada <petro.strynada@gmail.com>
2023-07-30 18:50:42 +0300

    Scores on the board: SKLabelNode
    + score count
    + edit mode (where you can add some boxes on the screan)
    + fix bouncers and slots

--- Project11_Pachinko/Project11_Pachinko/GameScene.swift
+++ Project11_Pachinko/Project11_Pachinko/GameScene.swift
@@ -8,6 +8,25 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var scoreLabel: SKLabelNode!
+
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score \(score)"
+        }
+    }
+
+    var editLabel: SKLabelNode!
+
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
 
     override func didMove(to view: SKView) {
         // Create a sprite node with the "background" image
@@ -21,40 +40,64 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         backgroundNode.blendMode = .replace
 
         // Set the zPosition to a very low value to make sure it's displayed as the background
-        backgroundNode.zPosition = -1
+        backgroundNode.zPosition = -10
 
         // Add the background node to the scene
         addChild(backgroundNode)
+
+        scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 2222, y: 1420)
+        addChild(scoreLabel)
+
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.horizontalAlignmentMode = .left
+        editLabel.position = CGPoint(x: 44, y: 1420)
+        addChild(editLabel)
 
         // Add physics borders for screen edges
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
 
         physicsWorld.contactDelegate = self
 
-        
         addLineOfBouncersAndSlots(count: 12, bouncerStep: 206, yCoordinate: 0)
+
+
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
+        let object = nodes(at: location)
 
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        ball.physicsBody?.restitution = 0.4
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-        ball.position = location
-        ball.name = "ball"
-        addChild(ball)
+        if object.contains(editLabel) {
+            editingMode.toggle()
+        } else {
+            if editingMode {
+                //create a box
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat(Int.random(in: 0...1)), green: CGFloat(Int.random(in: 0...1)), blue: CGFloat(Int.random(in: 0...1)), alpha: 1), size: size)
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+                addChild(box)
+
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                ball.physicsBody?.restitution = 0.4
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                ball.position = location
+                ball.name = "ball"
+                addChild(ball)
+            }
+        }
     }
 
-    func addBouncer(at position: CGPoint) {
-        let bouncer = SKSpriteNode(imageNamed: "bouncer")
-        bouncer.position = position
-        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
-        bouncer.physicsBody?.isDynamic = false
-        addChild(bouncer)
-    }
 
     func makeSlot(at position: CGPoint, isGood: Bool) {
 
@@ -71,30 +114,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             slotBase.name = "bad"
         }
 
-        slotBase.position = position
         slotGlow.position = position
+        slotGlow.zPosition = -9
+        slotBase.position = position
+        slotBase.zPosition = -8
 
         slotBase.physicsBody = SKPhysicsBody(rectangleOf: slotBase.size)
         slotBase.physicsBody?.isDynamic = false
 
-        addChild(slotBase)
         addChild(slotGlow)
+        addChild(slotBase)
 
         let spin = SKAction.rotate(byAngle: .pi, duration: 10)
         let spinForever = SKAction.repeatForever(spin)
         slotGlow.run(spinForever)
     }
 
-    func addLineOfBouncersAndSlots(count: Int, bouncerStep: Int, yCoordinate: Int ) {
 
-        var bouncerCount = 0
+    func addBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.zPosition = -7
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+
+
+    func addLineOfSlots(_ count: Int, _ bouncerStep: Int, _ yCoordinate: Int) {
         var slotCount = 1
         var slotStep = 0
-
-        for _ in 1...count {
-            addBouncer(at: CGPoint(x: bouncerStep * bouncerCount, y: yCoordinate))
-            bouncerCount += 1
-        }
 
         for _ in 1...count {
 
@@ -109,11 +158,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
     }
 
+    func addLineOfBouncers(_ count: Int, _ bouncerStep: Int, _ yCoordinate: Int) {
+        var bouncerCount = 0
+
+        for _ in 1...count {
+            addBouncer(at: CGPoint(x: bouncerStep * bouncerCount, y: yCoordinate))
+            bouncerCount += 1
+        }
+    }
+
+    func addLineOfBouncersAndSlots(count: Int, bouncerStep: Int, yCoordinate: Int) {
+        addLineOfSlots(count, bouncerStep, yCoordinate)
+        addLineOfBouncers(count, bouncerStep, yCoordinate)
+    }
+
     func collision(between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
 
